659a93254046664031431df98bd45abd04c253fb
Daniele Testa <daniele.testa@gmail.com>
2016-03-29 13:08:24 +0000

    Update script to support Kubernetes 1.2

--- install.sh
+++ install.sh
@@ -2,56 +2,94 @@
 set -o errexit
 set -o nounset
 
-# Send anonymous usage statistics to Scalr. Comment the next line to disable.
-curl "http://tools.scalr.club/counter/hit.php?id=4" || true
-
 # Check if running debian or redhat based OS
 if ! type "apt-get" 2>&1 > /dev/null; then
   OS="REDHAT"
+
+cat > /etc/yum.repos.d/kubernetes.repo <<EOL
+[virt7-docker-common-release]
+name=virt7-docker-common-release
+baseurl=http://cbs.centos.org/repos/virt7-docker-common-release/x86_64/os/
+gpgcheck=0
+EOL
+
+yum -y install --enablerepo=virt7-docker-common-release kubernetes etcd
+
+cat > /etc/kubernetes/config <<EOL
+KUBE_ETCD_SERVERS="--etcd-servers=http://master.k8s:2379"
+KUBE_LOGTOSTDERR="--logtostderr=true"
+KUBE_LOG_LEVEL="--v=0"
+KUBE_ALLOW_PRIV="--allow-privileged=false"
+EOL
+
 else
   OS="DEBIAN"
 fi
 
 if [ "$K8S_ROLE" == "MASTER" ]; then
 
+  echo -e "${SCALR_INTERNAL_IP} master.k8s" >> /etc/hosts
+  szradm queryenv set-global-variable scope=farm param-name=K8S_MASTER param-value="${SCALR_INTERNAL_IP}"
+
   if [ "$OS" == "REDHAT" ]; then
-    yum -y install etcd kubernetes
-    sed -i 's/ETCD_LISTEN_CLIENT_URLS="http:\/\/localhost:2379"/ETCD_LISTEN_CLIENT_URLS="http:\/\/0.0.0.0:2379"/g' /etc/etcd/etcd.conf
-    sed -i 's/KUBE_API_ADDRESS="--address=127.0.0.1"/KUBE_API_ADDRESS="--address=0.0.0.0"/g' /etc/kubernetes/apiserver
 
-    for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler; do 
-      systemctl restart $SERVICES
-      systemctl enable $SERVICES
-      systemctl status $SERVICES 
-    done
+cat > /etc/kubernetes/apiserver <<EOL
+KUBE_API_ADDRESS="--address=0.0.0.0"
+KUBE_API_PORT="--port=8080"
+KUBE_MASTER="--master=http://master.k8s:8080"
+KUBELET_PORT="--kubelet-port=10250"
+KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
+KUBE_API_ARGS=""
+EOL
 
-    etcdctl mk /atomic.io/network/config '{"Network":"172.17.0.0/16"}'
+cat > /etc/etcd/etcd.conf <<EOL
+ETCD_NAME=default
+ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
+ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379"
+ETCD_ADVERTISE_CLIENT_URLS="http://master.k8s:2379"
+EOL
+
+	for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler; do
+		systemctl restart $SERVICES
+		systemctl enable $SERVICES
+		systemctl status $SERVICES 
+	done
+
+    etcdctl mk /kubernetes.io/network/config '{"Network":"172.17.0.0/16"}'
   else
     echo "Ubuntu"
   fi
 
-  szradm queryenv set-global-variable scope=farm param-name=K8S_MASTER param-value="${SCALR_INTERNAL_IP}"
 
 else
 
+  echo -e "${K8S_MASTER} master.k8s" >> /etc/hosts
+
   if [ "$OS" == "REDHAT" ]; then
-    yum -y install flannel kubernetes
 
-    sed -i "s/FLANNEL_ETCD=\"http:\/\/127.0.0.1:2379\"/FLANNEL_ETCD=\"http:\/\/${K8S_MASTER}:2379\"/g" /etc/sysconfig/flanneld
-    sed -i "s/KUBE_MASTER=\"--master=http:\/\/127.0.0.1:8080\"/KUBE_MASTER=\"--master=http:\/\/${K8S_MASTER}:8080\"/g" /etc/kubernetes/config
-    echo 'KUBELET_PORT="--port=10250"' >> /etc/kubernetes/kubelet
-    sed -i "s/KUBELET_HOSTNAME=\"--hostname_override=127.0.0.1\"/KUBELET_HOSTNAME=\"--hostname_override=${SCALR_INTERNAL_IP}\"/g" /etc/kubernetes/kubelet
-    sed -i "s/KUBELET_API_SERVER=\"--api_servers=http:\/\/127.0.0.1:8080\"/KUBELET_API_SERVER=\"--api_servers=http:\/\/${K8S_MASTER}:8080\"/g" /etc/kubernetes/kubelet
+cat > /etc/kubernetes/kubelet <<EOL
+KUBELET_ADDRESS="--address=0.0.0.0"
+KUBELET_PORT="--port=10250"
+KUBELET_HOSTNAME="--hostname-override=${SCALR_INTERNAL_IP}"
+KUBELET_API_SERVER="--api-servers=http://master.k8s:8080"
+KUBELET_ARGS=""
+EOL
 
-    for SERVICES in kube-proxy kubelet docker flanneld; do 
-      systemctl restart $SERVICES
-      systemctl enable $SERVICES
-      systemctl status $SERVICES 
-    done
+    yum -y install flannel
+
+cat > /etc/sysconfig/flanneld <<EOL
+FLANNEL_ETCD="http://${K8S_MASTER}:2379"
+FLANNEL_ETCD_KEY="/kubernetes.io/network"
+EOL
+
+	for SERVICES in kube-proxy kubelet docker; do
+    	systemctl restart $SERVICES
+    	systemctl enable $SERVICES
+    	systemctl status $SERVICES 
+	done
 
   else
     echo "Ubuntu"
   fi
 
 fi
-
